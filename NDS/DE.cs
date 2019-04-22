9d35db5808040e999a107fcaa67ec54981efd6ee
DmitryNakhimovich <gun958833@yandex.ru>
2019-04-22 18:39:08 +0300

    update

--- NDS/DE.cs
+++ NDS/DE.cs
@@ -25,23 +25,62 @@ namespace NDS
         public double phi;
         public double gamma;
     }
-    public abstract class BaseDE
-    {        
-        public SystemParams sysParam;
-        public IMethod method;
 
-        /// Начальные значения системы
-        /// <param name="N">размер системы</param>
-        /// <param name="SysParam"> h, h1, lambda, p, mu0, mu, R, E, phi, gamma </param>
-        public BaseDE(int n, SystemParams p)
-        {
-            method.init(n);
-            SetParam(p);
-        }      
-        public void SetParam(SystemParams p)
+    public abstract class IMethodFunction
+    {
+        public SystemParams sysParam;
+
+        public IMethodFunction(SystemParams p)
         {
             sysParam = p;
         }
+
+        public abstract List<double> calculate(double t, List<double> Y);
+    }
+    public class DE1_Function : IMethodFunction
+    {
+        public DE1_Function(SystemParams p) : base(p)
+        {
+        }
+
+        public override List<double> calculate(double t, List<double> Y)
+        {
+            List<double> funcValue = new List<double>();
+            funcValue.Add(Y[1]);
+            funcValue.Add(-2 * sysParam.h * Y[1] - sysParam.p);
+            return funcValue;
+        }
+    }
+    public class DE2_Function : IMethodFunction
+    {
+        public DE2_Function(SystemParams p) : base(p)
+        {
+        }
+
+        public override List<double> calculate(double t, List<double> Y)
+        {
+            List<double> funcValue = new List<double>();
+            funcValue.Add(Y[1]);
+            funcValue.Add(-2 * sysParam.h1 * Y[1] - sysParam.lambda * sysParam.lambda * Y[0] - sysParam.p);
+            return funcValue;
+        }
+    }
+
+    public abstract class BaseDE
+    {
+        public SystemParams sysParam;
+        public IMethod method;
+        protected IMethodFunction function;
+        public List<double> time;
+        public List<List<double>> result;
+
+        public BaseDE(SystemParams _sysParam, int n)
+        {
+            sysParam = _sysParam;
+            time = new List<double>();
+            result = new List<List<double>>();
+        }
+
         /// <summary>
         /// Послеударные уравнения скоростей
         /// </summary>
@@ -49,14 +88,31 @@ namespace NDS
         /// <param name="U">доударное x'-</param>
         /// <param name="U1">доударное x'1-</param>
         /// <returns></returns>
-        abstract public double CalcHit(double t, double U, double U1);
-        public double F(double t)
+        abstract public double getHit(double t, double U, double U1);
+
+        public void setState(double _t, List<double> _res)
+        {
+            time.Clear();
+            result.Clear();
+                        
+            time.Add(_t);
+            result.Add(_res);
+            method.setState(_t, _res);
+        }
+        public void getNextStep()
+        {
+            method.getNextStep();
+            time.Add(method.t);
+            result.Add(method.result);
+        }
+
+        public double getF(double t)
         {
             double F1 = sysParam.E - sysParam.mu * Math.Cos(t);
             double F2 = -sysParam.mu * sysParam.gamma * Math.Cos(t - sysParam.phi);
             return Math.Max(F1, F2);
         }
-        public double Fdt(double t)
+        public double getFdt(double t)
         {
             double F1 = sysParam.E - sysParam.mu * Math.Cos(t);
             double F2 = -sysParam.mu * sysParam.gamma * Math.Cos(t - sysParam.phi);
@@ -69,92 +125,56 @@ namespace NDS
     }
     public class DE1 : BaseDE
     {
-        public DE1(int n, SystemParams p) : base(n, p)
+        public DE1(SystemParams _sysParam, int n) : base(_sysParam, n)
         {
+            function = new DE1_Function(sysParam);
+            method = new MethodRK4(function, n);
         }
 
-        /// Описание системы
-        /// <param name="t">Время</param>
-        /// <param name="Y">Решение</param>
-        /// <returns>Правая часть</returns>
-        protected override double[] Calculate(double t, double[] Y)
+        public override double getHit(double t, double U, double U1)
         {
-            // система уравнений модели
-            FY[0] = Y[1];
-            FY[1] = -2 * h * Y[1] - p;
-
-            return FY;
-        }
-        public override double CalcHit(double t, double Y, double Y1)
-        {
-            return ((mu0 - R) * Y + (1 + R) * Y1 + (1 + R) * Fdt(t)) / (1 + mu0);
+            return ((sysParam.mu0 - sysParam.R) * U + (1 + sysParam.R) * U1 + (1 + sysParam.R) * getFdt(t)) / (1 + sysParam.mu0);
         }
     }
-
     public class DE2 : BaseDE
     {
-        public DE2(int N, double[] SysParam) : base(N, SysParam)
+        public DE2(SystemParams _sysParam, int n) : base(_sysParam, n)
         {
+            function = new DE2_Function(sysParam);
+            method = new MethodRK4(function, n);
         }
 
-        /// Описание системы
-        /// <param name="t">Время</param>
-        /// <param name="Y">Решение</param>
-        /// <returns>Правая часть</returns>
-        protected override double[] Calculate(double t, double[] Y)
+        public override double getHit(double t, double U, double U1)
         {
-            // система уравнений модели
-            FY[0] = Y[1];
-            FY[1] = -2 * h1 * Y[1] - lambda * lambda * Y[0] - p;
-
-            return FY;
-        }
-        public override double CalcHit(double t, double Y, double Y1)
-        {
-            return (mu0 * (1 + R) * Y + (1 - R * mu0) * Y1 - mu0 * (1 + R) * Fdt(t)) / (1 + mu0);
+            return (sysParam.mu0 * (1 + sysParam.R) * U + (1 - sysParam.R * sysParam.mu0) * U1 - sysParam.mu0 * (1 + sysParam.R) * getFdt(t)) / (1 + sysParam.mu0);
         }
     }
 
     public class SystemDE
     {
-        // объекты диф. ур.
         public DE1 de1;
         public DE2 de2;
-        public double t;
-        public double[,] res;
 
-        public SystemDE(int N, double[] sysParam)
+        public SystemDE(SystemParams _sysParam, int n)
         {
-            de1 = new DE1(N, sysParam);
-            de2 = new DE2(N, sysParam);
-            t = 0;
+            de1 = new DE1(_sysParam, n);
+            de2 = new DE2(_sysParam, n);
         }
 
-        public void SetParam(double[] sysParam)
+        public void setParam(SystemParams _sysParam)
         {
-            de1.SetParam(sysParam);
-            de2.SetParam(sysParam);
+            de1.sysParam = _sysParam;
+            de1.method.function.sysParam = _sysParam;
+            de2.sysParam = _sysParam;
+            de2.method.function.sysParam = _sysParam;
         }
 
-        public double[] GetRes(int resIndex)
+        public void solveDiffs(List<double> initStateDE1, List<double> initStateDE2, double dt, double T)
         {
-            double[] tmp = new double[res.GetLength(1)];
-            for (int i = 0; i < res.GetLength(1); i++)
-            {
-                tmp[i] = res[resIndex, i];
-            }
-            return tmp;
-        }
+            de1.setState(0, initStateDE1);
+            de2.setState(0, initStateDE2);
+            int i = 0;
 
-        //вычисление
-        public void SolveDiffs(double[] initState, double dt, double T)
-        {
-            res = new double[4, (int)(T / dt + 1)];
-
-            de1.SetInit(0, initState);
-            de2.SetInit(0, initState);
-            int j = 0;
-            double t = 0;
             while (t <= T)
             {
                 de1.NextStep(dt);
--- NDS/Methods.cs
+++ NDS/Methods.cs
@@ -16,6 +16,7 @@ namespace NDS
     {
         double dt { get; set; }
         double eps { get; set; }
+
         void init(double _dt, double _eps, int n);
         void clear(int n);
     }
@@ -24,12 +25,14 @@ namespace NDS
         public double dt { get; set; }
         public double eps { get; set; }
 
+        /// <summary>
+        /// значения аргумента для i-ой производной
+        /// </summary>
         public List<double> YY { get; set; }
         public List<double> Y1 { get; set; }
         public List<double> Y2 { get; set; }
         public List<double> Y3 { get; set; }
         public List<double> Y4 { get; set; }
-        public List<double> FY { get; set; }
 
         public MethodStateRK4(int n)
         {
@@ -43,8 +46,8 @@ namespace NDS
             Y2 = new List<double>(n);
             Y3 = new List<double>(n);
             Y4 = new List<double>(n);
-            FY = new List<double>(n);
         }
+
         public void init(double _dt, double _eps, int n)
         {
             if (n < 0)
@@ -57,7 +60,6 @@ namespace NDS
             Y2 = new List<double>(n);
             Y3 = new List<double>(n);
             Y4 = new List<double>(n);
-            FY = new List<double>(n);
         }
         public void clear(int n)
         {
@@ -69,47 +71,6 @@ namespace NDS
             Y2 = new List<double>(n);
             Y3 = new List<double>(n);
             Y4 = new List<double>(n);
-            FY = new List<double>(n);
-        }
-    }
-
-    public abstract class IMethodFunction
-    {
-        public SystemParams sysParam;
-
-        public IMethodFunction(SystemParams p)
-        {
-            sysParam = p;
-        }
-
-        public abstract List<double> calculate(double t, List<double> Y);
-    }
-    public class DE1_Function : IMethodFunction
-    {
-        public DE1_Function(SystemParams p) : base(p)
-        {
-        }
-
-        public override List<double> calculate(double t, List<double> Y)
-        {
-            List<double> funcValue = new List<double>();
-            funcValue.Add(Y[1]);
-            funcValue.Add(-2 * sysParam.h * Y[1] - sysParam.p);
-            return funcValue;
-        }
-    }
-    public class DE2_Function : IMethodFunction
-    {
-        public DE2_Function(SystemParams p) : base(p)
-        {
-        }
-
-        public override List<double> calculate(double t, List<double> Y)
-        {
-            List<double> funcValue = new List<double>();
-            funcValue.Add(Y[1]);
-            funcValue.Add(-2 * sysParam.h1 * Y[1] - sysParam.lambda * sysParam.lambda * Y[0] - sysParam.p);
-            return funcValue;
         }
     }
 
@@ -130,7 +91,7 @@ namespace NDS
         /// <summary>
         /// модель уравнений системы
         /// </summary>
-        protected IMethodFunction function;
+        public IMethodFunction function;
 
         /// Выделение памяти под рабочие массивы
         /// <param name="func">Рассчетная модель системы</param>
@@ -139,6 +100,8 @@ namespace NDS
         {
             function = func;
         }
+
+        abstract public void setState(double _t, List<double> _res);
 
         /// Вычислить следующий шаг
         /// <param name="dt">текущий шаг по времени</param>
@@ -158,7 +121,7 @@ namespace NDS
             t = 0;
         }
 
-        public void setState(double _t, List<double> _res)
+        public override void setState(double _t, List<double> _res)
         {
             t = _t;
             result = _res;
@@ -168,31 +131,27 @@ namespace NDS
         public override void getNextStep()
         {
             int i = 0;
+            int size = result.Count;
             if (state.dt < 0) return;
 
             state.Y1 = function.calculate(t, result);
-            for (i = 0; i < Y.Length; i++)
-                YY[i] = Y[i] + Y1[i] * (dt / 2.0);
 
-            // рассчитать Y2
-            Y2 = Calculate(t + dt / 2.0, YY);
-            for (i = 0; i < Y.Length; i++)
-                YY[i] = Y[i] + Y2[i] * (dt / 2.0);
+            for (i = 0; i < size; i++)
+                state.YY[i] = result[i] + state.Y1[i] * (state.dt / 2.0);
+            state.Y2 = function.calculate(t + state.dt / 2.0, state.YY);
 
-            // рассчитать Y3
-            Y3 = Calculate(t + dt / 2.0, YY);
-            for (i = 0; i < Y.Length; i++)
-                YY[i] = Y[i] + Y3[i] * dt;
+            for (i = 0; i < size; i++)
+                state.YY[i] = result[i] + state.Y2[i] * (state.dt / 2.0);
+            state.Y3 = function.calculate(t + state.dt / 2.0, state.YY);
 
-            // рассчитать Y4
-            Y4 = Calculate(t + dt, YY);
+            for (i = 0; i < size; i++)
+                state.YY[i] = result[i] + state.Y3[i] * state.dt;
+            state.Y4 = function.calculate(t + state.dt, state.YY);
 
-            // рассчитать решение на новом шаге
-            for (i = 0; i < Y.Length; i++)
-                Y[i] = Y[i] + dt / 6.0 * (Y1[i] + 2.0 * Y2[i] + 2.0 * Y3[i] + Y4[i]);
+            for (i = 0; i < size; i++)
+                result[i] = result[i] + (state.dt / 6.0) * (state.Y1[i] + 2.0 * state.Y2[i] + 2.0 * state.Y3[i] + state.Y4[i]);
 
-            // рассчитать следующее время
-            t += dt;
+            t += state.dt;
         }
     }
 
